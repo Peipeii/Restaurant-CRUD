d5c62486ddbeb56a3a7c977adec72c6f13fc666d
Peipeii <pey728@gmail.com>
2021-06-03 16:23:27 -0700

    feat: modify route settings ('/') & modify index.hbs

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-//require packages used in the project
+// require packages used in the project
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const Restaurant = require('./models/restaurant.js') //load restaurant model
 
+// define express server variables
 const app = express()
 const port = 3000
+
 
 // setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -12,12 +16,37 @@ app.set('view engine', 'hbs')
 // setting static files
 app.use(express.static('public'))
 
+// db connection
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
 // routes setting
+// get all restaurant
 app.get('/', (req, res) => {
-  res.render('index')
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
+})
+
+// get a restaurant
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  console.log('req.params.restaurant_id', req.params.restaurant_id)
+})
+
+app.get('/restaurants/search', (req, res) => {
+  const keyword = req.query.keyword
+  res.render('index', { restaurants, keyword })
 })
 
 // start and listen on the Express server
 app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`)
+  console.log(`App is running on http://localhost:${port}`)
 })
